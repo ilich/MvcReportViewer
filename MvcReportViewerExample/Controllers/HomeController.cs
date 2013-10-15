5e86e59300ac3ca79ce5c5a95bcfb1163b99635c
Ilya Verbitskiy <iverbitskiy@gmail.com>
2013-10-15 10:41:04 -0500

    Completed sample web-site

--- MvcReportViewerExample/Controllers/HomeController.cs
+++ MvcReportViewerExample/Controllers/HomeController.cs
@@ -1,10 +1,15 @@
 ﻿using System.Web.Mvc;
 
-namespace MvcReportViewer.Example
+namespace MvcReportViewer.Example.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Fluent()
         {
             return View();
         }
