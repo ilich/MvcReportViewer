68258ca418b88f05f66360883cc5e74e5819eb2a
Ilya Verbitskiy <iverbitskiy@gmail.com>
2014-01-20 00:49:53 +0100

    Packaging v0.3.0

--- MvcReportViewerExample/Properties/AssemblyInfo.cs
+++ MvcReportViewerExample/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MvcReportViewer")]
-[assembly: AssemblyCopyright("Copyright © Ilya Verbitskiy 2013")]
+[assembly: AssemblyCopyright("Copyright © Ilya Verbitskiy 2013-2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.2.*")]
-[assembly: AssemblyFileVersion("0.2.1.0")]
+[assembly: AssemblyVersion("0.3.*")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
--- MvcReportViewerTests/Properties/AssemblyInfo.cs
+++ MvcReportViewerTests/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MvcReportViewer")]
-[assembly: AssemblyCopyright("Copyright © Ilya Verbitskiy 2013")]
+[assembly: AssemblyCopyright("Copyright © Ilya Verbitskiy 2013-2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.*")]
-[assembly: AssemblyFileVersion("0.2.1.0")]
+[assembly: AssemblyVersion("0.3.*")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
