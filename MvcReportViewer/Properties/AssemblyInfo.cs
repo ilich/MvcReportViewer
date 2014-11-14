96760e713dcc120a7190a4ead6f7b9b9922d9dfc
Ilya Verbitskiy <iverbitskiy@gmail.com>
2014-11-14 16:27:47 -0600

    Release v0.4.2

--- MvcReportViewer/Properties/AssemblyInfo.cs
+++ MvcReportViewer/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.4.*")]
-[assembly: AssemblyFileVersion("0.4.1.0")]
+[assembly: AssemblyFileVersion("0.4.2.0")]
 [assembly: InternalsVisibleTo("MvcReportViewerTests")]
