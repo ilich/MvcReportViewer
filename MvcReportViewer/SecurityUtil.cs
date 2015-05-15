d747f51d998a864421ec535caaee0b02075acbc7
Ilya Verbitskiy <iverbitskiy@gmail.com>
2015-05-15 09:29:30 -0500

    #72 Use new API  for parameters encryption

--- MvcReportViewer/SecurityUtil.cs
+++ MvcReportViewer/SecurityUtil.cs
@@ -11,15 +11,42 @@ namespace MvcReportViewer
         public static string Encrypt(string value)
         {
             var bytes = Encoding.Unicode.GetBytes(value);
+#if NET45
+            var encryptedBytes = MachineKey.Protect(bytes);
+            var encrypted = BitConverter.ToString(encryptedBytes).Replace("-", "");
+#else
             var encrypted = MachineKey.Encode(bytes, MachineKeyProtection.Encryption);
+#endif
             return encrypted;
         }
 
         public static string Decrypt(string encrypted)
         {
+#if NET45
+            var encryptedBytes = Hex2Bytes(encrypted);
+            var bytes = MachineKey.Unprotect(encryptedBytes);
+#else
             var bytes = MachineKey.Decode(encrypted, MachineKeyProtection.Encryption);
+#endif
             var value = Encoding.Unicode.GetString(bytes);
             return value;
+        }
+
+        private static byte[] Hex2Bytes(string hex)
+        {
+            if (hex.Length % 2 != 0)
+            {
+                throw new ArgumentException(hex);
+            }
+
+            var bytes = new byte[hex.Length / 2];
+            for(var i = 0; i < hex.Length; i += 2)
+            {
+                var encByte = hex.Substring(i, 2);
+                bytes[i / 2] = Convert.ToByte(encByte, 16);
+            }
+
+            return bytes;
         }
     }
 }
