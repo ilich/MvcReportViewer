bd88af1e270bf39c7bcee77d612e1d1d54e257f6
Ilya Verbitskiy <iverbitskiy@gmail.com>
2015-05-20 09:46:18 -0500

    #84. Download report as PDF if user wants to print it in Firefox or Chrome

--- MvcReportViewerExample/Scripts/MvcReportViewer.js
+++ MvcReportViewerExample/Scripts/MvcReportViewer.js
@@ -24,27 +24,7 @@
         }
 
         function printReport() {
-            var styles = $('#ReportViewer_ctl09_ReportControl_styles').html();
-            styles = 'body{ margin: 10px; padding: 0; } ' + styles;
-
-            var report = $('#ReportViewer_ctl09').html();
-
-            // Open report popup
-            var popup = window.open("", "_blank", "location=no, menubar=no, resizable=yes, scrollbars=yes, status=no, toolbar=no, title=yes, widht=300, height=500");
-            var content = popup.document;
-            content.open();
-            content.write(
-                '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">' +
-                '<html>' +
-                '<head><title>Printing Report</title><style type="text/css">' +
-                styles +
-                '</style></head>' +
-                '<body onload="window.print();">' +
-                report +
-                '</body></html>');
-
-            content.close();
-            popup.focus();
+            $find('ReportViewer').exportReport('PDF');
         }
 
         // 1. Fix Report Scrolling in IE 10 and IE 11
