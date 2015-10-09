245e6dcd6f6ded7213d93bcd315ba9877a70cc6d
Ilya Verbitskiy <iverbitskiy@gmail.com>
2015-10-08 22:37:20 -0500

    Release 0.7.4

--- MvcReportViewerExample/Properties/AssemblyInfo.cs
+++ MvcReportViewerExample/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("0.7.*")]
-[assembly: AssemblyFileVersion("0.7.3.0")]
+[assembly: AssemblyFileVersion("0.7.4.0")]
