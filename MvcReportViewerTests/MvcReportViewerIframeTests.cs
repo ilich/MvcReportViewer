00d0fb6485b91c6e0ede103c4b6417314bacae9f
Ilya Verbitskiy <iverbitskiy@gmail.com>
2016-03-07 16:50:41 -0600

    #168. Updated NuGet packages and moved to NUnit 3.

--- MvcReportViewerTests/MvcReportViewerIframeTests.cs
+++ MvcReportViewerTests/MvcReportViewerIframeTests.cs
@@ -10,7 +10,7 @@ namespace MvcReportViewer.Tests
     {
         private readonly HtmlHelper _htmlHelper = HtmlHelperFactory.Create();
 
-        [TestFixtureSetUp]
+        [OneTimeSetUp]
         public void Setup()
         {
             MvcReportViewerIframe.ApplyAppPathModifier = p => p;
